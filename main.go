2fd8db313561be15ef976b1be23256cd5e9499e9
Chris McArthur <prince.chrismc@gmail.com>
2020-12-12 17:21:53 -0500

    getting started with AWS go SDK
    https://www.pulumi.com/docs/get-started/aws/

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "io/ioutil"
+	"github.com/pulumi/pulumi-aws/sdk/v3/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an AWS resource (S3 Bucket)
+bucket, err := s3.NewBucket(ctx, "my-bucket", &s3.BucketArgs{
+    Website: s3.BucketWebsiteArgs{
+        IndexDocument: pulumi.String("index.html"),
+    },
+})
+
+htmlContent, err := ioutil.ReadFile("site/index.html")
+if err != nil {
+    return err
+}
+
+_, err = s3.NewBucketObject(ctx, "index.html", &s3.BucketObjectArgs{
+    Acl:         pulumi.String("public-read"),
+    ContentType: pulumi.String("text/html"),
+    Bucket:      bucket.ID(),
+    Content:     pulumi.String(string(htmlContent)),
+})
+if err != nil {
+    return err
+}
+
+		// Export the name of the bucket
+		ctx.Export("bucketName", bucket.ID())
+                ctx.Export("bucketEndpoint", pulumi.Sprintf("http://%s", bucket.WebsiteEndpoint))
+		return nil
+	})
+}
